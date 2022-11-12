d7fdc12b7e48a51e1f2743dc55518341d0fdcf4e
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-13 00:16:10 +0530

    moduel pattern -> commonjs module and es module

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const printDetail = require("./printDetail.js");
+
+
+
 
 // console.log(process);
 let sum = Number(process.argv[2]) + Number(process.argv[3]);
 // console.log(sum);
 
 
-console.log(__dirname);
+
+
+// console.log(__dirname);
+
+
+
+
+// console.log(module);
+printDetail.printName("Harshit Raj")
+printDetail.printAge(25);
