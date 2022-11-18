c8e9b2f95139b97623cb476e04221887458b82a7
Harshit Raj <41303856+harshit-8080@users.noreply.github.com>
2022-11-19 02:21:22 +0530

    ˀcosuming world cup teams

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("server");
+const axios = require("axios");
+
+async function f1(){
+    
+    const teams = await axios.get("http://localhost:3000/team/teams");
+    return teams.data;
+
+}
+
+f1()
+.then((v)=>{
+    console.log(JSON.stringify(v,null,3));
+})
+
